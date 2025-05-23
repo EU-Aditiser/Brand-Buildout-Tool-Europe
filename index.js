8bcc18e4a2a074e573d23ccf83690606260973b4
Jamshid <jamshid@example.com>
2025-05-23 17:36:48 +1200

    Remove all old gapi.auth2 authentication logic; use only GIS

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /** 
  * Initialize Buttons 
  * */
-const authorizeButton = document.getElementById('authorize_button');
 const signoutButton = document.getElementById('signout_button');
 const createBrandBuildoutTemplateButton = document.getElementById('create_brand_buildout_template_button');
 const accountBuildoutButton = document.getElementById('create_account_buildout_button');
@@ -13,77 +12,6 @@ updateButtonState(BUTTON_STATE);
 let ACCOUNTS = [];
 let accessToken = null;
 
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-
-    // Initiate Buttons
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-    createBrandBuildoutTemplateButton.onclick = handleCreateBrandTemplateBuildoutClick;
-    accountBuildoutButton.onclick = handleAccountBuildoutClick;
-
-    // Initiate patch notes
-    const patchNotes = readTextFile("patchnotes.txt")
-    document.getElementById('patch_notes').innerText=patchNotes;    
-    
-  }, function(error) {
-    console.error("Google API client init error:", error);
-  });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
-  }
-}
-
-// =======================
-// Button Handling
-// =======================
-
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
 
 /**
  * 
