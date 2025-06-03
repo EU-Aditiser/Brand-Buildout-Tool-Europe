6009e4d0884afbd6cee02ca69ed5eb29144dcd48
Jamshid <jamshid@example.com>
2025-06-03 22:45:39 +1200

    Remove unused synchronous readTextFile function

--- index.js
+++ index.js
@@ -188,24 +188,6 @@ function readAccountBuildoutData() {
   return formData;
 }
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-            }
-        }
-    }
-    rawFile.send(null);
-    return rawFile.responseText;
-}
-
 // Initialize Google API client
 function initGoogleApiClient() {
   return new Promise((resolve, reject) => {
